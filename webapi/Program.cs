bb5ce46b1dd21dc25c2fb69c15e336d50295b43d
Kyle Karthauser <kkarthauser@assurity.com>
2023-08-21 10:07:58 -0500

    Formatting

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -2,9 +2,6 @@ using System.Text.Json.Serialization;
 using webapi.Service;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,29 +13,19 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
-            policy =>
-            {
-                policy.WithOrigins("https://localhost:5002");
-            });
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:5002");
+        });
 });
 
 builder.Services.AddManagerDependencies();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
-
-
 app.Run();
